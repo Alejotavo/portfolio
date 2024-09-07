d9b9f24dc9b9351c9187a7bbf955b10c2e624bc5
Alejo <alejogustavof@gmail.com>
2024-09-07 07:39:08 -0300

    add profile image

--- src/pages/home/mainHeader/mainHeader.tsx
+++ src/pages/home/mainHeader/mainHeader.tsx
@@ -1,15 +1,23 @@
+import { Col, Row } from "react-bootstrap";
+import profile from '../../../assets/profile_pic.png'; // Importa la imagen
 
 function MainHeader() {
     return (
     <section>
-        <aside>
-            <h1>Welcome to My Portfolio</h1>
-            <h3>ALEJO GUSTAVO FRANCOMANO</h3>
-            <p>Lorem Ipsum segoe merdask laralala</p>
-        </aside>
-        <div>
-            aca va la imagen mia
-        </div>
+        <Row>
+            <Col>
+                <aside>
+                    <h1>Welcome to My Portfolio</h1>
+                    <h3>ALEJO GUSTAVO FRANCOMANO</h3>
+                    <p>Lorem Ipsum segoe merdask laralala</p>
+                </aside>
+            </Col>
+            <Col>
+                <div>
+                <img src={profile} alt="Profile" />
+                </div>
+            </Col>
+        </Row>
     </section>
    );
   }
