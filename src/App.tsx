364076ef5cca0ae8f951e75a7fe874f14d31cb03
Alejotavo <alejogustavof@gmail.com>
2024-09-07 05:04:59 -0300

    add routing and page section

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import './App.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import About from "./pages/about";
 
 function App() {
   return (
-    <Container fluid className='no-margins'>
-          <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-      <h1 className="text-center my-4">Este es mi Portfolio</h1>
-      <div className="text-center">
-        <button className="btn btn-success">Warning</button>
+    <Container fluid className="no-margins">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="*" />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
       </div>
-      <p className="text-center mt-4">
-        Click on the Vite and React logos to learn more
-      </p>
     </Container>
   );
 }
