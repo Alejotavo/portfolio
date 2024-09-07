afcf0624d07ae6535eb0a9b0fe70536a21c1364a
Alejo <alejogustavof@gmail.com>
2024-09-07 06:57:28 -0300

    change about page folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Container from "react-bootstrap/Container";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import About from "./pages/about";
+import About from "./pages/about/about";
 import NavBar from "./components/nav/nav";
 
 function App() {
