5409009b57abfc7326735535ae4b6cdeb0a4d5d4
Alejo <alejogustavof@gmail.com>
2024-09-07 08:20:55 -0300

    add aside section

--- src/pages/home/mainHeader/mainHeader.tsx
+++ src/pages/home/mainHeader/mainHeader.tsx
@@ -1,19 +1,20 @@
 import { Col, Row } from "react-bootstrap";
 import profile from '../../../assets/profile_pic.png'; // Importa la imagen
+import "./mainHeader.scss"
 
 function MainHeader() {
     return (
     <section>
-        <Row>
+        <Row g-0>
             <Col>
                 <aside>
-                    <h1>Welcome to My Portfolio</h1>
+                    <h1>Welcome to my <b>Portfolio</b></h1>
                     <h3>ALEJO GUSTAVO FRANCOMANO</h3>
-                    <p>Lorem Ipsum segoe merdask laralala</p>
+                    <p>At any rate, Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque amet, quod doloribus the framework of the essence represents a bond between the flexible production planning</p>
                 </aside>
             </Col>
             <Col>
-                <div>
+                <div  className="col d-flex justify-content-center">
                 <img src={profile} alt="Profile" />
                 </div>
             </Col>
