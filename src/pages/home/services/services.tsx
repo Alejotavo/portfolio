58a54d1f2b03394e6e6d1d49ba37b18b5cd005cc
Alejo <alejogustavof@gmail.com>
2024-09-09 09:49:34 -0300

    working on services section

--- src/pages/home/services/services.tsx
+++ src/pages/home/services/services.tsx
@@ -1,29 +1,29 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import "./services.scss"
+import Card from "./card/card";
 
 function Services() {
     return (
-        <Container id="section1" fluid className="custom-container">
+        <Container id="section1" fluid className="custom-container ">
             <Row className="custom-row">
                <Col xs={12}>
-                <label>MY SERVICES</label>
-                <h3>What I Do?</h3>
+                <label className="skyblue-subtitle">MY SERVICES</label>
+                <h3 className="grey-subtitle">What I Do?</h3>
                 <p>At any rate, Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque amet, quod doloribus the framework of the essence represents a bond between the flexible production planning</p>
                </Col>
-                <Row className="custom-row">
-                    <Col xs={12} md={4}>
-                    <label>MY SERVICES</label>
-                    <p>At any rate, Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </Col>
-                    <Col xs={12} md={4}>
-                    <label>MY SERVICES</label>
-                    <p>At any rate, Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </Col>
-                    <Col xs={12} md={4}>
-                    <label className="col col-md-4">MY SERVICES</label>
-                    <p>At any rate, Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </Col>
-                </Row>
+                <Col  xs={12}>
+                    <Row className="custom-row">
+                        <Col xs={12} md={4}>
+                            <Card/>
+                        </Col>
+                        <Col xs={12} md={4}>
+                            <Card/>
+                        </Col>
+                        <Col xs={12} md={4}>
+                            <Card/>
+                        </Col>
+                    </Row>
+                </Col>
             </Row>
         </Container>
    );
