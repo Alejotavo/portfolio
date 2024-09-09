2df3acd52747927d9c96845671d003f19df9a10a
Alejo <alejogustavof@gmail.com>
2024-09-08 21:35:59 -0300

    change col css

--- src/pages/home/services/services.tsx
+++ src/pages/home/services/services.tsx
@@ -5,21 +5,21 @@ function Services() {
     return (
         <Container fluid className="custom-container">
             <Row className="custom-row">
-               <Col xs={12} className="col-12">
+               <Col xs={12}>
                 <label>MY SERVICES</label>
                 <h3>What I Do?</h3>
                 <p>At any rate, Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque amet, quod doloribus the framework of the essence represents a bond between the flexible production planning</p>
                </Col>
                 <Row className="custom-row">
-                    <Col className="col col-md-4">
+                    <Col xs={12} md={4}>
                     <label>MY SERVICES</label>
                     <p>At any rate, Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     </Col>
-                    <Col className="col col-md-4">
+                    <Col xs={12} md={4}>
                     <label>MY SERVICES</label>
                     <p>At any rate, Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     </Col>
-                    <Col>
+                    <Col xs={12} md={4}>
                     <label className="col col-md-4">MY SERVICES</label>
                     <p>At any rate, Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     </Col>
