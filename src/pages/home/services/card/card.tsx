22b4fe847307f0d4e42af5bec6af0983fc4a6f31
Alejo <alejogustavof@gmail.com>
2024-09-09 09:59:14 -0300

    add styles to services cards

--- src/pages/home/services/card/card.tsx
+++ src/pages/home/services/card/card.tsx
@@ -1,6 +1,8 @@
+import "./card.scss"
+
 function Card() {
     return (
-        <div className="card mt-1 mb-1">
+        <div className="card-layout mt-1 mb-1">
         <div className="card-body">
             <h5 className="card-title">Web Development</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
