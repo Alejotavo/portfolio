f8238de72d47c9c4df29be841659af0d022fd912
Alejo <alejogustavof@gmail.com>
2024-09-10 07:53:27 -0300

    add progress bar animation

--- src/pages/home/skills/progress.tsx
+++ src/pages/home/skills/progress.tsx
@@ -1,18 +1,51 @@
-
+import React, { useState, useEffect } from 'react';
 import { ProgressBar } from 'react-bootstrap';
-import {Skill} from '../../../models/skill'
+import { Skill } from '../../../models/skill';
+import { useInView } from 'react-intersection-observer';
 
 interface ChildComponentProps {
-    skill: Skill[]
+    skill: Skill[];
 }
 
 const Progress = ({ skill }: ChildComponentProps) => {
+    // Estado para las barras de progreso animadas
+    const [progressValues, setProgressValues] = useState<{ [key: number]: number }>({});
     
+    // Hook para detectar si el componente está en vista
+    const { ref, inView } = useInView({
+        triggerOnce: true, // Solo ejecutar la animación una vez
+        threshold: 0.1, // El componente debe estar al menos al 10% visible
+    });
+
+    useEffect(() => {
+        if (inView) {
+            // Función para animar el progreso
+            const animateProgress = () => {
+                skill.forEach((x, index) => {
+                    setProgressValues(prev => {
+                        const current = prev[index] || 0;
+                        if (current < x.value) {
+                            return { ...prev, [index]: Math.min(current + 1, x.value) };
+                        } else {
+                            return prev;
+                        }
+                    });
+                });
+            };
+
+            const interval = setInterval(animateProgress, 20); // Ajusta el intervalo según tu necesidad
+
+            // Limpieza del intervalo cuando se completa la animación
+            const cleanup = () => clearInterval(interval);
+            return cleanup;
+        }
+    }, [inView, skill]);
+
     return (
-        <div>
+        <div ref={ref}>
             {skill.map((x, index) => (
                 <div
-                     key={index}
+                    key={index}
                     className="col-12 mb-3"
                     data-aos="fade-up"
                     data-aos-delay={index * 100}
@@ -20,14 +53,13 @@ const Progress = ({ skill }: ChildComponentProps) => {
                 >
                     <label className="form-label">{x.title}</label>
                     <div>
-                        <span className="progress-label">{`${x.value}%`}</span>
-                        <ProgressBar now={x.value} />
+                        <span className="progress-label">{`${progressValues[index] || 0}%`}</span>
+                        <ProgressBar now={progressValues[index] || 0} />
                     </div>
-
                 </div>
             ))}
         </div>
-    )
-}
-  export default Progress;
-  
+    );
+};
+
+export default Progress;
