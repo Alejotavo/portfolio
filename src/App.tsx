92c129007c9b059d062c0e05bd4a6e0efc83fdbd
Alejo <alejogustavof@gmail.com>
2024-09-11 15:56:55 -0300

    add Main portfolio landing page

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Home from "./pages/home/home";
 
 import 'aos/dist/aos.css'; // Importa los estilos de AOS
 import AOS from 'aos'; // Importa AOS
+import MainPortfolioPage from "./pages/mainPortfolio/mainPortfolio";
 
 
 // Inicializa AOS
@@ -26,6 +27,7 @@ function App() {
           <Routes>
             <Route path="*" />
             <Route path="/home" element={<Home />} />
+            <Route path="/portfolio" element={<MainPortfolioPage />} />
             <Route path="/portfolioDetails/:id" element={<PortfolioDetails />} />
             <Route path="/about" element={<About />} />
           </Routes>
---
+++ src/pages/mainPortfolio/mainPortfolio.tsx
@@ -0,0 +1,13 @@
+import Portfolio from "../home/portfolio/portfolio";
+import './mainPortfolio.scss'
+
+function MainPortfolioPage() {
+    return (
+      <main>
+       <Portfolio/>
+      </main>
+      
+    );
+  }
+  export default MainPortfolioPage;
+  
