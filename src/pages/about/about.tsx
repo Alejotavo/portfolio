29f0ba272aa43c4897bece080cf48e5f4ace059f
Alejo <alejogustavof@gmail.com>
2024-09-12 19:02:29 -0300

    add about section padding paragraph

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -12,7 +12,7 @@ function About() {
                 </div>
             </Col>
             <Col className="col-12 col-md-6">
-                <aside data-aos="fade-right" className="custom-padding mt-md-5 pt-md-5 ">
+                <aside data-aos="fade-right" className=" mt-md-5 pt-md-5 pe-md-5">
                     <h1 className="mb-5">Find out more about me</h1>
                     <label className="name mt-0 mb-0">ALEJO GUSTAVO FRANCOMANO</label>
                     <label className="ui-ux mt-1 mb-3">UI-UX & FRONT END DEVELOPER</label>
