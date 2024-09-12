43a02eac3fd70007e3e3a4bab7295ef8b5bc36c7
Alejo <alejogustavof@gmail.com>
2024-09-12 11:22:49 -0300

    refactor padding services container

--- src/pages/home/services/services.tsx
+++ src/pages/home/services/services.tsx
@@ -17,7 +17,7 @@ function Services() {
 
     return (
         <Container id="section2" fluid className="custom-container">
-            <Row className="custom-row">
+            <Row className="custom-row mt-5">
                <Col xs={12} xl={6} className="row ms-0">
                 <label className="skyblue-subtitle">MY SERVICES</label>
                 <h3 className="white-subtitle">What I Do?</h3>
