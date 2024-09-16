68216d21f6478fbcb3b515c0249b1ef2c4a43aee
Alejo <alejogustavof@gmail.com>
2024-09-16 19:17:16 -0300

    about section add padding text

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -12,14 +12,13 @@ function About() {
                 </div>
             </Col>
             <Col className="col-12 col-md-6">
-                <aside data-aos="fade-right" className=" mt-md-5 pt-md-5 pe-md-5">
+                <aside data-aos="fade-right" className=" mt-md-5 pt-md-5 pe-md-5 p-3">
                     <h1 className="mb-5">Find out more about me</h1>
                     <div className="name mt-0 mb-0">ALEJO GUSTAVO FRANCOMANO</div>
                     <label className="ui-ux mt-1 mb-3 fw-bold">UI-UX & FRONT END DEVELOPER</label>
                     <p>
                     I am a Front-End Developer with a focus on UI/UX, passionate about creating intuitive and visually engaging digital experiences. My work sits at the crossroads of design and functionality, using technologies like React and frameworks such as Bootstrap to develop responsive and optimized interfaces. I enjoy collaborating with multidisciplinary teams to solve complex problems creatively, always keeping the user at the center of the development process.
-
-With a keen eye for detail, I continuously strive to improve my skills and stay updated on the latest design and technology trends, ensuring that every project is modern and effective.
+                    With a keen eye for detail, I continuously strive to improve my skills and stay updated on the latest design and technology trends, ensuring that every project is modern and effective.
                     </p>
                 </aside>
             </Col>
