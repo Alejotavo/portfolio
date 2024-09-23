af24d4449f119290b8e6caf1497bf69b0e458fc0
Alejo <alejogustavof@gmail.com>
2024-09-23 19:34:19 -0300

    refactor logo svg

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -4,26 +4,26 @@ import "./logo.scss"
 function Logo() {
     return (
     <>
-        <svg width="65" height="64" viewBox="0 0 78 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M40.036 45.9614C50.6494 45.9614 59.2533 37.3575 59.2533 26.7441C59.2533 16.1307 50.6494 7.52679 40.036 7.52679C37.4518 7.52679 34.9868 8.03686 32.7361 8.96182C35.8971 9.6492 39.0043 12.3832 40.2002 15.3028C41.8632 19.363 39.9199 24.0026 35.8597 25.6656C31.7995 27.3287 27.1599 25.3854 25.4969 21.3252C24.7013 19.3829 24.5663 16.9058 25.0863 14.6679C22.4173 17.9679 20.8187 22.1693 20.8187 26.7441C20.8187 37.3575 29.4226 45.9614 40.036 45.9614Z" fill="url(#paint0_linear_339_141)"/>
+        <svg width="78" height="64" viewBox="0 0 78 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" clipRule="evenodd" d="M40.036 45.9614C50.6494 45.9614 59.2533 37.3575 59.2533 26.7441C59.2533 16.1307 50.6494 7.52679 40.036 7.52679C37.4518 7.52679 34.9868 8.03686 32.7361 8.96182C35.8971 9.6492 39.0043 12.3832 40.2002 15.3028C41.8632 19.363 39.9199 24.0026 35.8597 25.6656C31.7995 27.3287 27.1599 25.3854 25.4969 21.3252C24.7013 19.3829 24.5663 16.9058 25.0863 14.6679C22.4173 17.9679 20.8187 22.1693 20.8187 26.7441C20.8187 37.3575 29.4226 45.9614 40.036 45.9614Z" fill="url(#paint0_linear_339_141)"/>
         <path d="M24.3419 5.60504C18.0642 8.67981 5.49828 16.1746 0 19.5376C6.13886 17.082 17.6799 11.9467 23.0607 10.4094C29.7868 8.48764 39.8759 8.32749 42.7585 8.48764C45.6411 8.64778 50.2852 8.96807 59.4134 13.4521C66.716 17.0393 74.3068 24.1284 77.1894 27.2245C72.7588 22.0465 62.5523 10.6336 57.1714 6.40576C50.4454 1.12101 46.4418 0 42.1179 0C37.794 0 32.189 1.76158 24.3419 5.60504Z" fill="#010101"/>
         <path d="M20.8187 39.3954C15.2604 35.9346 7.15309 27.8651 3.84344 24.1818C9.9665 29.5199 23.5032 40.6766 33.7904 43.8795C44.0775 47.0824 56.6534 45.1073 61.0149 43.5592C58.0255 45.0005 50.4454 47.755 44.0396 47.2425C36.0324 46.6019 29.3064 44.6802 20.8187 39.3954Z" fill="#010101"/>
         <g filter="url(#filter0_f_339_141)">
         <ellipse cx="40.5" cy="59" rx="24.5" ry="3" fill="url(#paint1_radial_339_141)"/>
         </g>
         <defs>
-        <filter id="filter0_f_339_141" x="14.9" y="54.9" width="51.2" height="8.2" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <filter id="filter0_f_339_141" x="14.9" y="54.9" width="51.2" height="8.2" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <feFlood floodOpacity="0" result="BackgroundImageFix"/>
         <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
         <feGaussianBlur stdDeviation="0.55" result="effect1_foregroundBlur_339_141"/>
         </filter>
         <linearGradient id="paint0_linear_339_141" x1="25.7832" y1="11.6905" x2="55.7301" y2="41.7976" gradientUnits="userSpaceOnUse">
-        <stop offset="0.335" stop-color="#79D9E7"/>
-        <stop offset="1" stop-color="#5169B9"/>
+        <stop offset="0.335" stopColor="#79D9E7"/>
+        <stop offset="1" stopColor="#5169B9"/>
         </linearGradient>
         <radialGradient id="paint1_radial_339_141" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(40.5 59) scale(24.5 3)">
-        <stop stop-color="#D9D9D9"/>
-        <stop offset="1" stop-color="white"/>
+        <stop stopColor="#D9D9D9"/>
+        <stop offset="1" stopColor="white"/>
         </radialGradient>
         </defs>
         </svg>
