65db840c7a2de6d94bf1557d13a265905bb69b7d
Alejo <alejogustavof@gmail.com>
2025-06-10 12:30:59 -0300

    delete class on mainHeader section

--- src/pages/home/mainHeader/mainHeader.tsx
+++ src/pages/home/mainHeader/mainHeader.tsx
@@ -11,7 +11,7 @@ function MainHeader() {
              <ParticlesBackground />
             </div>
             <Col className="col-12 col-md-6">
-                <aside data-aos="fade-right" className="custom-padding ">
+                <aside data-aos="fade-right">
                     <h1 className="mb-5">Welcome to my <b>Portfolio</b></h1>
                     <label className="name mt-0 mb-0">ALEJO GUSTAVO FRANCOMANO</label>
                     <label className="ui-ux mt-1 mb-3">UI-UX & FRONT END DEVELOPER</label>
